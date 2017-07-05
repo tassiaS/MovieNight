4da3a12732a51f67f89c4d7716be903480d04267
Tassia Serrao <tassia@lucass-macbook-air.local>
2017-07-05 22:58:02 +0200

    Initial Commit

---
+++ MovieNight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by Tassia Serrao on 05/07/2017.
+//  Copyright © 2017 Tassia Serrao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
