a82794969a0b712f2d7fdaf9b319d7671088ee1a
Tassia Serrao <tassia@Lucass-MacBook-Air.local>
2017-07-06 19:13:36 +0200

    Home and SelectGenre UI

--- MovieNight/MovieNightModel.swift
+++ MovieNight/MovieNightModel.swift
@@ -8,6 +8,30 @@
 
 import Foundation
 
+//Model
+
+protocol People {
+    var name: String { get }
+    var movies: [Movie] { get }
+}
+
+struct Actor: People {
+    var name: String
+    var movies: [Movie]
+}
+
+struct Director: People {
+    var name: String
+    var movies: [Movie]
+}
+
 struct Movie {
-    
+    var title: String
+    var releaseDate: String
+    var voteAverage: Int
+}
+
+struct Genre {
+    var name: String
+    var movies: [Movie]
 }
