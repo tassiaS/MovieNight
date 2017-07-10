21527ea748b0026d2a0da71ec8ac0be401c2b8b7
Tassia Castro <tassiaserrao@gmail.com>
2017-07-10 15:40:04 +0200

    Results UI with Auto Layout

---
+++ MovieNight/ResultTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResultTableViewCell.swift
+//  MovieNight
+//
+//  Created by Tassia Serrao on 10/07/2017.
+//  Copyright © 2017 Tassia Serrao. All rights reserved.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
